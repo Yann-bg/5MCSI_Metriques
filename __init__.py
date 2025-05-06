2791cb7f3b7822eb4a15bb50e4912dd7bcfc2056
Yann-bg <yannkueti@gmail.com>
2025-05-06 17:49:02 +0200

    Update __init__.py
    rien du tout faites pas gaffe

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ from urllib.request import urlopen
 import sqlite3
 
                                                                                                                                        
-app = Flask(__name__)            #yo   
+app = Flask(__name__)             
 
 @app.route("/contact/")
 def MaPremiereAPI():
